b85631988462f1e12c8d0212bf6e98de29ba0c88
Pung Pin Yuan <sebastian.ppy@gmail.com>
2025-03-22 15:24:14 +0800

    3rd deployable version

--- file_for_streamlit/app.py
+++ file_for_streamlit/app.py
@@ -5,8 +5,8 @@ import joblib
 from datetime import datetime, timedelta
 
 # === Load model + encoder ===
-model = joblib.load("/home/sebastian/code/JPYY-96/travel_insider/searchdate/interface/final_regression_model_22mar.joblib")
-encoder = joblib.load("/home/sebastian/code/JPYY-96/travel_insider/searchdate/interface/encoder.joblib")
+model = joblib.load("final_regression_model_22mar.joblib")
+encoder = joblib.load("encoder.joblib")
 
 st.title("✈️ Best Flight Booking Date Advisor")
 st.markdown("Select your route and flight date to find the best day to book.")
