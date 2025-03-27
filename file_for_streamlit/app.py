d3d19782bd6a0bba80ae9fcf93de0b4567ca9284
Pung Pin Yuan <sebastian.ppy@gmail.com>
2025-03-27 19:49:15 +0800

    Update app.py

--- file_for_streamlit/app.py
+++ file_for_streamlit/app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
-from tensorflow.keras.models import load_model
 from datetime import datetime, timedelta
 
 # === Load model + encoder ===
